7c734e1806991e7c0a21de919f7bfd1b2bae248b
SHUBHAM KUMAR <shubhamkmr06082001@gmail.com>
2023-07-27 12:17:16 +0530

    Added

---
+++ day4-Java/hello.java
@@ -0,0 +1,26 @@
+// Compile - javac className.java
+// run - java className
+
+public class hello {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    Vehicle car = new Vehicle();
+    car.notify();
+  }
+}
+
+class safan {
+  public static void main(String[] args) {
+    System.out.println("Hello safan");
+  }
+}
+
+class Vehicle {
+  int wheels = 4;
+}
+
+class Rohit {
+  public static void main(String[] args) {
+    System.out.println("Hello Rohit");
+  }
+}
